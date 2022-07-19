e492391d7aaa0fa326f7b5ffd96e97b91b4393cc
ThallesKraft <90985985+ThallesKraft@users.noreply.github.com>
2022-07-19 09:51:48 -0300

    Create dependencias.js

---
+++ extra/dependencias.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const fs = require("fs");
+
+const c = require("colors");
+
+const jogos = require("discord-gamecord");
+
+const moment = require("moment");
+
+const chatdevoz = require("@discordjs/voice");
+
+const traduzir = require("@vitalets/google-translate-api");
+
+const mongo = require("mongoose");
+
+const akinator = require("discord.js-akinator");
+
+const Discord = require("discord.js");
+
+const { DiscordTogether } = require('discord-together');
