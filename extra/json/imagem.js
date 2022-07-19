95b389e57fd36ae63e3d0bae3e13318ea1846847
ThallesKraft <90985985+ThallesKraft@users.noreply.github.com>
2022-07-19 09:52:22 -0300

    Create imagem.js

---
+++ extra/json/imagem.js
@@ -0,0 +1,4 @@
+{  
+  "avatar": "https://cdn.discordapp.com/avatars/958834427058847844/d62dd128832a9567eb5cc44f99e690cb.png?size=2048",
+  "gartilho": "https://cdn.discordapp.com/attachments/984891912366469151/995042235785678858/b_config.png"
+}
